25a3daafd342f02d78a8da3146695d17a4935398
KHDIJAED <khadija.eddraiji@gmail.com>
2024-02-21 07:01:28 -0800

    updated

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Deletes the database hbtn_0c_0 from my MySQL server.
+sh: 1: q: not found
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Creates a table called first_table with values id and name in my MySQL server.
+sh: 1: q: not found
